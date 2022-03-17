a72e9bf0c2c27fcc12731472c2e41cbeea6f2e4d
Benson <bensonbird2@gmail.com>
2022-03-17 01:28:25 -0600

    PassTheTurn

--- Winston.Lib/CardProcessor.cs
+++ Winston.Lib/CardProcessor.cs
@@ -97,7 +97,7 @@ namespace Winston
             }
             else if (enumSet == Set.NEO && stringRarity == "common")
             {
-                url = $"{General.BaseUrl()}cards?rarity=common&set=neo&type=creature&type=artifact&type=enchantment";
+                url = $"{General.BaseUrl()}cards?rarity=common&set=neo&type=creature|type=artifact|type=enchantment";
 
                 using (HttpResponseMessage response = await General.ApiHelper.ApiClient.GetAsync(url))
                 {
@@ -112,7 +112,7 @@ namespace Winston
                     }
                 }
 
-                url = $"{General.BaseUrl()}cards?rarity=common&set=neo&type=sorcery&type=instant";
+                url = $"{General.BaseUrl()}cards?rarity=common&set=neo|type=sorcery|type=instant";
             }
             else
             {
