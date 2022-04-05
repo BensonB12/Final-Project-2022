f3a2f298bc316b05ae9d0aa3bcc30e67b5ae002c
Benson <bensonbird2@gmail.com>
2022-04-05 12:34:20 -0600

    DragNDropWorks!

---
+++ Blazor/Models/CardModelHTML.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Winston
+{
+    public class CardModelHTML
+    {
+        private CardStatus status = CardStatus.Pile1FaceUp;
+        public string Id { get; set; }
+        public CardModel card { get; set; }
+        public CardStatus Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+        // public string Description { get; set; }
+        public string StringURL { get; set; }
+        public string BackOfCardURL => "BackOfMTGCard.jpg";
+        //public DateTime LastUpdated { get; set; }
+    }
+
+    public enum CardStatus
+    {
+        Pile1FaceUp,
+        Pile2FaceDown
+    }
+}
