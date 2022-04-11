89a2c8bda5b99e86a001c4128e3aea4215061f0e
Benson <bensonbird2@gmail.com>
2022-04-11 09:37:25 -0600

    FOFWorks

--- Winston.Lib/StaticCards.cs
+++ Winston.Lib/StaticCards.cs
@@ -4,7 +4,7 @@ namespace Winston
     {
         private static int player = 1;
         public static int Player
-        { 
+        {
             get
             {
                 return player;
@@ -16,9 +16,12 @@ namespace Winston
         }
         public static bool Using { get; set; }
         public static List<CardModel> PileOfCards { get; set; }
-        public static List<CardModel> PopUpCards = new List<CardModel>();
+        public static (List<CardModel>, List<CardModel>) PopUpCards = (new List<CardModel>(), new List<CardModel>());
         public static bool BoolPopUpCards = false;
         public static int chosenPlayer = 1;
         public static bool otherPlayerCanSee = false;
+        public static List<CardModel> Player1Cards = new List<CardModel>();
+        public static List<CardModel> Player2Cards = new List<CardModel>();
+
     }
 }
