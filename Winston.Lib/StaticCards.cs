01d3ab63c2dbac2cb5b836d07369cb39963df022
Benson <bensonbird2@gmail.com>
2022-04-20 09:19:35 -0600

    FixedFOFButtons

--- Winston.Lib/StaticCards.cs
+++ Winston.Lib/StaticCards.cs
@@ -20,6 +20,7 @@ namespace Winston
         public static bool BoolPopUpCards = false;
         public static int chosenPlayer = 1;
         public static bool otherPlayerCanSee = false;
+        public static bool onlyOnePlayer = true;
         public static List<CardModel> Player1Cards = new List<CardModel>();
         public static List<CardModel> Player2Cards = new List<CardModel>();
         public static bool GameOver = false;
